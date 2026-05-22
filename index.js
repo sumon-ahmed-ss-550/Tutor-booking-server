450a426dadc74cffeb11b93b39ea0328f58f2773
sumon550 <entertainmentcharacter54321@gmail.com>
2026-05-23 03:04:21 +0600

    Add all method for action

--- index.js
+++ index.js
@@ -24,12 +24,62 @@ const run = async () => {
 
     const database = client.db("Tutor_booking");
     const collection = database.collection("tutors-data");
+    const bookingCollection = database.collection("booking-data");
 
     // get all data
+    // app.get("/tutors", async (req, res) => {
+    //   const cursor = collection.find().limit(6);
+    //   const allValues = await cursor.toArray();
+    //   res.send(allValues);
+    // });
+
     app.get("/tutors", async (req, res) => {
-      const cursor = collection.find();
-      const allValues = await cursor.toArray();
-      res.send(allValues);
+      try {
+        const { search, after, before } = req.query;
+
+        let query = {};
+
+        // SEARCH BY NAME OR SUBJECT
+        if (search) {
+          query.$or = [
+            {
+              tutorName: {
+                $regex: search,
+                $options: "i",
+              },
+            },
+            {
+              subject: {
+                $regex: search,
+                $options: "i",
+              },
+            },
+          ];
+        }
+
+        // DATE RANGE FILTER
+        if (after || before) {
+          query.sessionStartDate = {};
+
+          if (after) {
+            query.sessionStartDate.$gte = after;
+          }
+
+          if (before) {
+            query.sessionStartDate.$lte = before;
+          }
+        }
+
+        const result = await collection.find(query).limit(6).toArray();
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: "Failed to fetch tutors",
+          error: error.message,
+        });
+      }
     });
 
     // get one data
@@ -39,6 +89,98 @@ const run = async () => {
         _id: new ObjectId(id),
       };
       const result = await collection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/tutors/user/:userId", async (req, res) => {
+      const { userId } = req.params;
+      const result = await collection.find({ userId: userId }).toArray();
+      res.send(result);
+    });
+
+    // post data of database
+    app.post("/tutors", async (req, res) => {
+      const data = req.body;
+      const result = await collection.insertOne(data);
+      res.send(result);
+    });
+
+    // post booking data
+    app.post("/tutors/booking", async (req, res) => {
+      try {
+        const data = req.body;
+
+        // 1. booking insert
+        const bookingResult = await bookingCollection.insertOne(data);
+
+        // 2. tutors collection থেকে slot কমানো
+        await collection.updateOne(
+          { _id: new ObjectId(data.tutorId) },
+          {
+            $inc: { totalSlot: -1 },
+          },
+        );
+
+        res.send({
+          success: true,
+          bookingResult,
+        });
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: "Booking failed",
+          error: error.message,
+        });
+      }
+    });
+
+    // get booking data
+    app.get("/tutors/booking/:userId", async (req, res) => {
+      const { userId } = req.params;
+      const result = await bookingCollection.find({ userId: userId }).toArray();
+      res.send(result);
+    });
+
+    // update cancelled booking
+    app.patch("/booking/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const result = await bookingCollection.updateOne(
+          { _id: new ObjectId(id) },
+          {
+            $set: { status: "Cancelled" },
+          },
+        );
+
+        res.send({
+          success: true,
+          modifiedCount: result.modifiedCount,
+        });
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: "Failed to update booking",
+          error: error.message,
+        });
+      }
+    });
+
+    app.patch("/tutors/user/:userId", async (req, res) => {
+      const { userId } = req.params;
+      const data = req.body;
+      const filter = { userId: userId };
+      const updateDoc = {
+        $set: data,
+      };
+      const result = await collection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // delete user
+    app.delete("/tutors/user/:userId", async (req, res) => {
+      const { userId } = req.params;
+      const result = await collection.deleteOne({ userId: userId });
       res.send(result);
     });
 
